c807d533c20d4b24f376407a7ed7b65bb8119ea3
Syldriem <downelliot@gmail.com>
2023-02-03 21:13:55 +0100

    parsed anotha one

--- src/bin/parseone.rs
+++ src/bin/parseone.rs
@@ -19,6 +19,9 @@ impl Record {
     fn get_id(&self) -> &String {
         return &self.company_id;
     }
+    fn get_car(&self) -> &String {
+        return &self.car;
+    }
 }
 
 impl Borrow<String> for Record {
@@ -38,15 +41,37 @@ fn example() -> Result<(), Box<dyn Error>> {
         let res: Record = result?;
         vec.push(res);
     };
-    let order_map = count_orders_per_company(vec);
+    let order_map = count_orders_per_company(&vec);
     let mut sorted: Vec<_> = order_map.iter().collect();
     sorted.sort_by(|a, b| b.1.cmp(a.1));
-    println!("{:?}", sorted);
+    let car_map = count_orders_per_car_brand(vec);
+    let mut s_car_map: Vec<_> = car_map.iter().collect();
+    s_car_map.sort_by(|a, b| b.1.cmp(a.1));
+    println!("{:?}\n", sorted);
+    println!("{:?}", s_car_map);
     return Ok(());
 }
+fn count_orders_per_car_brand(orders: Vec<Record>) -> HashMap<String, i32>{
+    let mut orders_per_company = HashMap::new();
+    for order in orders {
+        let car = order.get_car();
+        if orders_per_company.contains_key(order.get_car()) {
+            let idx = orders_per_company.get_mut(car);
+            match idx {
+                Some(val) => {
+                    let newval = *val;
+                    _ = orders_per_company.insert(car.to_owned(), newval + 1)
+                },
+                None => println!("this should never happend"),
+            }
+        } else {
+            orders_per_company.insert(car.to_owned(), 1);
+        }
+    };
+    return orders_per_company;
+}
 
-
-fn count_orders_per_company(orders: Vec<Record>) -> HashMap<String, i32>{
+fn count_orders_per_company(orders: &Vec<Record>) -> HashMap<String, i32>{
     let mut orders_per_company = HashMap::new();
 
     for order in orders {
