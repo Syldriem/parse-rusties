9810a57d367aa5357fd4f577285d013013f4227f
Syldriem <downelliot@gmail.com>
2023-02-14 13:23:11 +0100

    stuff

---
+++ src/options.rs
@@ -0,0 +1,16 @@
+Error: Custom { kind: InvalidData, error: Error("unknown variant `ApiVersionCheck`, expected `ListMacros` or `ExpandMacro`", line: 1, column: 18) }\n
+
+#[derive(Clap)]
+struct Struct {
+    args: String[],
+    pwd: String,
+}
+
+
+
+
+
+
+
+
+
